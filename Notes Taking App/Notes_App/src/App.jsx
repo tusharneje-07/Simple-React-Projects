7b013022c011cfffe6447f5172aa54f25d24028a
Tushar <nejetushar07@gmail.com>
2025-06-01 10:22:04 +0530

    1:Note taking App - Adding Functionality of Adding and Deleting Note

--- Notes Taking App/Notes_App/src/App.jsx
+++ Notes Taking App/Notes_App/src/App.jsx
@@ -1,28 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react'
+import Note from './Note'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [curDateTime, setCurDateTime] = useState(new Date());
+  const [Notes, setNotes] = useState(JSON.parse(localStorage.getItem('userNotes')) || {});
+  const [enteredNote, setEnteredNote] = useState('Enter Note Title....');
+  const [enteredNoteTitle, setEnteredNoteTitle] = useState('Enter Note Content....');
+
+
+  const handleReadNote = useCallback(() => {
+    let data = localStorage.getItem('userNotes');
+    if (data) {
+      const parsedData = JSON.parse(data);
+      setNotes(parsedData);
+      console.log(`Notes read from localStorage: ${JSON.stringify(parsedData)}`);
+
+    } else {
+      setNotes({});
+    }
+  }, [Notes]);
+
+
+  const handleAddNote = useCallback((note) => {
+    const data = localStorage.getItem('userNotes');
+    let userNotes = data ? JSON.parse(data) : {};
+    const date = new Date().toLocaleString('en-GB', { timeZone: 'Asia/Kolkata' });
+    const [d, m, y, h, min, s] = date.match(/\d+/g);
+    const key = `${d}-${m}-${y}-${h}-${min}-${s}`;
+    userNotes[key] = {};
+    userNotes[key].title = enteredNoteTitle;
+    userNotes[key].content = enteredNote;
+    userNotes[key].date = curDateTime.toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' });
+
+    localStorage.setItem('userNotes', JSON.stringify(userNotes));
+    setNotes(userNotes);
+    setEnteredNote('');
+    setEnteredNoteTitle('');
+    console.log(`Note added with key: ${key}`);
+    console.log(`Current notes: ${JSON.stringify(userNotes)}`);
+    handleReadNote();
+
+  }, [handleReadNote]);
+
+  setInterval(() => {
+    setCurDateTime(new Date());
+  }, 1000);
+
+  const handleDeleteNote = useCallback((key) => {
+    const data = localStorage.getItem('userNotes');
+    if (data) {
+      let userNotes = JSON.parse(data);
+      if (userNotes[key]) {
+        delete userNotes[key];
+        localStorage.setItem('userNotes', JSON.stringify(userNotes));
+        setNotes(userNotes);
+        console.log(`Note with key ${key} deleted`);
+      } else {
+        console.log(`No note found with key ${key}`);
+      }
+    } else {
+      console.log('No notes found in localStorage');
+    }
+  }, [Notes]);
 
   return (
     <>
-      <div className='w-full h-screen flex items-start justify-start bg-black/70'>
+      <div className='w-full h-screen flex items-start justify-start bg-black/80 py-10'>
 
         {/* Creating Notes */}
-        <div className='w-[20%] h-full flex flex-col items-start justify-start border-r-[1px] p-2'>
-            <label htmlFor="datetime" className='text-sm text-white mt-5'>12/12/12</label>
-            <input
-              type="text"
-              placeholder="Note Title"
-              className='w-full p-2 border border-gray-300 rounded mb-4 outline-none text-black'
-            />
-            <textarea name="note" id="note" className='w-full h-[200px] p-2 border border-gray-300 rounded mb-4 outline-none text-black' placeholder='Write your note here...'>
+        <div className='w-[20%] h-full flex flex-col items-start justify-start border-r-[1px] px-4'>
 
-            </textarea>
-            <button className='bg-blue-500 text-white bg-gray-300 hover:bg-gray-300/70 p-2 rounded text-gray-900/70 transition-all duration-600'>Add Note
-            </button>
+
+          <label htmlFor="datetime" className='text-sm text-white mb-2 mt-4 bg-gray-600 w-full p-2 rounded-lg text-left'>
+            {curDateTime.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })}
+          </label>
+
+          <input
+            value={enteredNoteTitle}
+            onInput={(e) => {
+              setEnteredNoteTitle(e.target.value);
+            }}
+            type="text"
+            placeholder="Note Title"
+            className='w-full p-2 rounded-lg mb-4 outline-none text-white bg-gray-600 focus:bg-gray-600/70 transition-all duration-600'
+          />
+          <textarea name="note" id="note" className='bg-gray-600 focus:bg-gray-600/70 w-full h-[200px] p-2 rounded-lg mb-4 outline-none text-white transition-all duration-600' placeholder='Write your note here...' onInput={(e) => {
+            setEnteredNote(e.target.value);
+          }}>
+            {enteredNote}
+          </textarea>
+          <button className='text-white bg-gray-600 hover:bg-gray-600/70 px-3 py-2 rounded-lg text-gray-900/80 transition-all duration-600 outline-none' onClick={handleAddNote} >Add Note
+          </button>
+        </div>
+
+
+
+        {/* A Card of Note */}
+
+        <div className='w-[80%] h-full flex flex-row items-start justify-start px-4 flex-wrap gap-1 overflow-auto overflow-x-hidden'>
+
+          <div className="columns-1 sm:columns-2 md:columns-4 gap-4 p-4 w-full">
+            {/* {notes.map((note, i) => (
+              <div key={i} className="w-full break-inside-avoid">
+                <Note note={note} />
+              </div>
+            ))} */}
+
+            {
+              Object.keys(Notes).map((key, i) => (
+                <div key={i} className="w-full break-inside-avoid">
+                  <Note note={[Notes[key], key]} deleteNote={(key) => {
+                    console.log(`Key to delete: ${key}`);
+                    handleDeleteNote(key)
+                  }} />
+                </div>
+              ))
+            }
+          </div>
+
+
+
         </div>
 
       </div>
---
+++ Notes Taking App/Notes_App/src/Note.jsx
@@ -0,0 +1,49 @@
+import React, { useRef, useEffect } from 'react';
+
+export default function NoteCard({ note,deleteNote }) {
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = 'auto';
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    }, []);
+
+    const handleInput = (e) => {
+        e.target.style.height = 'auto';
+        e.target.style.height = `${e.target.scrollHeight}px`;
+    };
+
+    return (
+        <div className="break-inside-avoid mb-4 w-full bg-gray-600 hover:bg-gray-600/70 p-4 rounded-lg flex flex-col transition-colors duration-300 relative">
+            <div className='flex items-center justify-between mb-2'>
+                <h2 className='text-white text-lg font-semibold'>{note[0].title}</h2>
+                <span className='text-gray-400 text-sm'>{note[0].date}</span>
+            </div>
+
+            <textarea
+                ref={textareaRef}
+                onInput={handleInput}
+                defaultValue={note[0].content}
+                className="text-gray-300 text-sm w-full bg-transparent border-none resize-none outline-none overflow-hidden text-left leading-relaxed mb-5"
+                rows={1}
+            />
+
+            <button className="rounded-tl-lg rounded-br-lg bg-red-100 hover:bg-red-200 w-min p-2 absolute bottom-0 right-0">
+                <svg 
+                onClick={(e) => {
+                    e.stopPropagation();
+                    console.log(`Delete note with key: ${note[1]}`);
+                    deleteNote(note[1]);
+
+                }}
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className='fill-black/80 w-5 h-5 hover:w-6 hover:h-6 hover:fill-black/90 transition-all duration-300'>
+                    <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
+                </svg>
+            </button>
+
+
+        </div>
+    );
+}
