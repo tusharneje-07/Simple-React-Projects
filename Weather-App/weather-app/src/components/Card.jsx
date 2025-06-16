9ea9b338615e58eca44cd7d5aafa54a89510806b
Tushar <nejetushar07@gmail.com>
2025-06-16 13:43:21 +0530

    Weather-App [7]

--- Weather-App/weather-app/src/components/Card.jsx
+++ Weather-App/weather-app/src/components/Card.jsx
@@ -1,51 +1,50 @@
 import React from 'react';
 import TimeCard from './TimeCard';
-import { getIconForWeatherCode,getShortWeatherDescription } from './Functions';
+import { getIconForWeatherCode, getShortWeatherDescription } from './Functions';
 const Card = ({ temperature = 0, code = 0, humidity = 0, hourly_data = {}, className }) => {
     return (
 
         <div className='flex flex-col items-center justify-center gap-4'>
-        
-        
 
-        <div className="duration-300 font-mono text-white group cursor-pointer relative overflow-hidden h-48 rounded-3xl p-4 w-56 
+            <div className="duration-300 font-mono text-white group cursor-pointer relative overflow-hidden h-48 rounded-3xl p-4 w-56 
   backdrop-blur-xl bg-white/10 border border-white/20 shadow-2xl hover:scale-105 hover:shadow-lg hover:bg-white/20">
 
-            <h3 className="text-xl text-center">Today</h3>
+                <h3 className="text-xl text-center">Today</h3>
 
-            <div className="gap-4 relative">
-                <img src={`/icons/${getIconForWeatherCode(code, true)}`} alt="" className="w-20 scale-[110%]" />
+                <div className="gap-4 relative">
+                    <img src={`/icons/${getIconForWeatherCode(code, true)}`} alt="" className="w-20 scale-[110%]" />
 
-                <h4 className="font-sans duration-300 absolute left-1/2 text-center -translate-x-2 -translate-y-16 text-5xl">
-                    {parseInt(temperature)}°C
-                </h4>
+                    {document.getElementById('web-icon')?.setAttribute('href', `/icons/${getIconForWeatherCode(code, true)}`)}
+
+                    <h4 className="font-sans duration-300 absolute left-1/2 text-center -translate-x-2 -translate-y-16 text-5xl">
+                        {parseInt(temperature)}°C
+                    </h4>
+                </div>
+
+                <div className="absolute duration-300 mt-2 font-sans left-10">
+                    <p className="text-sm">{getShortWeatherDescription(code)}</p>
+                    <p className="text-sm">{humidity}% humidity</p>
+                </div>
+
             </div>
 
-            <div className="absolute duration-300 mt-2 font-sans left-10">
-                <p className="text-sm">{getShortWeatherDescription(code)}</p>
-                <p className="text-sm">{humidity}% humidity</p>
+            <div className='w-screen flex'>
+                <div className='overflow-x-auto'>
+                    <div className='flex flex-row items-center gap-4 min-w-max p-4'>
+                        {Object.keys(hourly_data).length > 0 && hourly_data.time.map((time, index) => (
+                            <TimeCard
+                                key={index}
+                                date={time.split('T')[0].split('-').reverse().join('/')}
+                                time={time.split('T')[1].slice(0, 5)}
+                                temperature={hourly_data.temperature_2m[index]}
+                                code={hourly_data.weathercode[index]}
+                                className="flex-shrink-0"
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
 
-        </div>
-
-
-        {/* Uncomment this and make Hourly forcast */}
-        {console.log(hourly_data)}
-        
-        <div className='w-[500px] flex flex-row justify-center items-center gap-4 overflow-scroll p-4 m-4'>
-            {
-            Object.keys(hourly_data).length > 0 && hourly_data.time.map((time, index) => (
-                <TimeCard
-                    key={index}
-                    date={time.split('T')[0].split('-').reverse().join('/')}
-                    time={time.split('T')[1].slice(0, 5)}
-                    temperature={hourly_data.temperature_2m[index]}
-                    code={hourly_data.weathercode[index]}
-                    className="flex-shrink-0"
-                />
-            ))
-        }
-        </div>
 
 
         </div>
--- Weather-App/weather-app/src/components/TimeCard.jsx
+++ Weather-App/weather-app/src/components/TimeCard.jsx
@@ -2,16 +2,16 @@ import { getIconForWeatherCode,getShortWeatherDescription } from './Functions';
 
 export default function TimeCard({ date="22/06", time="12:00", temperature=0, code, className="" }) {
     return (
-        <div className={`duration-300 font-mono text-white cursor-pointer relative overflow-hidden w-28 h-48 rounded-3xl p-4 
-  backdrop-blur-xl bg-white/10 border border-white/20 shadow-2xl hover:scale-105 hover:shadow-lg hover:bg-white/20 ${className}`}>
+        <div className={`duration-300 font-mono text-white cursor-pointer relative overflow-hidden w-32 h-48 rounded-3xl p-4 
+  backdrop-blur-xl bg-white/10 border border-white/20 hover:scale-105 hover:shadow-lg hover:bg-white/20 ${className}`}>
 
             <h3 className="text-lg text-center font-sans">{date}</h3>
             <h3 className="text-sm text-center font-sans">{time}</h3>
 
-            <div className="gap-4 relative">
-                <img src={`/icons/${getIconForWeatherCode(parseInt(code), true)}`} alt="" className="w-20 scale-[105%]" />
+            <div className="gap-2 relative flex flex-col items-center justify-between h-min">
+                <img src={`/icons/${getIconForWeatherCode(parseInt(code), true)}`} alt="" className="w-20 scale-[115%]" />
 
-                <h4 className="font-sans duration-300 absolute left-1/2 -translate-x-1/2 -translate-y-2 text-3xl text-center">
+                <h4 className="font-sans duration-300 text-3xl text-center absolute left-1/2 -translate-x-1/2 translate-y-[4.8rem]">
                     {parseInt(temperature)}°C
                 </h4>
             </div>
